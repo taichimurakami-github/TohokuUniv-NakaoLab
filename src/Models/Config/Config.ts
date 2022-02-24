4591251f6b7f91726d8aa5682d362e4c3270ee8e
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2022-02-24 22:26:30 +0900

    Configクラス実装

---
+++ src/Models/Config/Config.ts
@@ -0,0 +1,38 @@
+import { type_AllConfig, type_VariantConfig } from "../../../@types/config";
+
+class Config {
+  private config: type_AllConfig;
+  constructor(config: type_AllConfig) {
+    this.config = config;
+  }
+
+  //まとめて複数のconfigを取る
+  public getAllConfig = () => this.config;
+  public getIOConfig = () => this.config.io;
+
+  //params
+  public getTimeLength = () => this.config.params.timeLength;
+  public getMaxTravelCoeffConst = () => this.config.params.maxCoeffConst;
+  public getMaxPopulationSize = () => this.config.params.maxPopulationSize;
+  public getInitialBirthRate = () => this.config.params.birthRate;
+  public getInitialInfectiousRate = () =>
+    this.config.params.initialInfectiousRate;
+  public getInitialFatality = () => this.config.params.initialFatarity;
+  public getFeedbackRate = () => this.config.params.feedbackRate;
+
+  //models
+  public getSpaceConnectionType = () => this.config.models.Space.connectionType;
+  public getSpaceLength = () => this.config.models.Space.length;
+  public getInitialPopulation = () =>
+    this.config.models.People.initialPopulation;
+  public getEI_transCoeff = () => this.config.models.People.EI_transCoeff;
+
+  //vaccine
+  public getVaccineSchedules = (n: number) =>
+    this.config.vaccine.begin[String(n)];
+  public getVaccineSetting = (vaccineName: string) =>
+    this.config.vaccine.data[vaccineName];
+
+  //variant
+  public getVirusSettings = () => this.config.variantConfig;
+}
--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,10 @@ export const main = async (
   flowName: string,
   stepName: string
 ) => {
-  /**
-   * (1)設定読み込み
-   */
-  // const settings = await IO.readFile(
-  //   path.resolve(__dirname, "../config/settings.json")
-  // );
-  // const variantConfig = await IO.readFile(
-  //   path.resolve(__dirname, "../config/vaccine.json")
-  // );
-  // const variantConfig = await IO.readFile(
-  //   path.resolve(__dirname, "../config/variant.json")
-  // );
-
-  /**
-   * (2)計算準備
-   */
+  //計算準備
   const s = new Space(config);
 
-  /**
-   * (3)計算
-   */
+  //計算
   const timeLength = config.params.timeLength;
   for (let t = 0; t < timeLength; t++) {
     config.io.showProgressBar &&
@@ -35,14 +18,10 @@ export const main = async (
     s.updateWithLifeCycle();
   }
 
-  /**
-   * (4)結果取得
-   */
+  //結果取得
   const result = s.getResults();
 
-  /**
-   * (5)書き出し
-   */
+  // 書き出し
   const writeFileTypes = [];
   config.io.writeResultAsXLSX && writeFileTypes.push("xlsx");
   config.io.writeResultAsJSON && writeFileTypes.push("json");
